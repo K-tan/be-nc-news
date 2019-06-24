be55c8c15a6dba073d12f5f0785f6811b6fc14a4
Katan Patel <katanpatel86@gmail.com>
2019-06-24 20:26:22 +0100

    formatDate updated

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,13 @@
-exports.formatDate = list => {};
+exports.formatDate = list => {
+  let formattedArr = list.map(ele => {
+    let date = new Date(ele.created_at);
+    ele.created_at = date;
+    return ele;
+  });
+  console.log(formattedArr);
+  return formattedArr;
+};
 
 exports.makeRefObj = list => {};
 
-exports.formatComments = (comments, articleRef) => {};
+exports.formatComments = (comments, articleRef) => {}; //call function inside function
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,8 +1,47 @@
-const { expect } = require('chai');
-const { formatDate, makeRefObj, formatComments } = require('../db/utils/utils');
+const { expect } = require("chai");
+const { formatDate, makeRefObj, formatComments } = require("../db/utils/utils");
 
-describe('formatDate', () => {});
+describe.only("formatDate", () => {
+  it("returns a new array of objects", () => {
+    const list = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    const actual = formatDate(list);
+    const expected = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    expect(actual).not.to.equal(expected);
+  });
+  it("converts the created_at value to a javascript date object", () => {
+    const list = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    actual = formatDate(list);
+    expect(actual[0].created_at).to.be.an.instanceOf(Date);
+  });
+});
 
-describe('makeRefObj', () => {});
+describe("makeRefObj", () => {});
 
-describe('formatComments', () => {});
+describe("formatComments", () => {});
