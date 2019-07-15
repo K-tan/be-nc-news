e86af9e1ca469a6336d0b8b6128ec7f45163a43c
Katan Patel <katanpatel86@gmail.com>
2019-07-15 14:08:04 +0100

    installation of cors

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const {
   handle404PSQLerrors,
   handle405Errors
 } = require("./errors");
+const cors = require("cors");
 //require error handlers in here
-
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
